ba166a2e4161ef7c47d28e57abadf05577128f09
Alicewish <anywaywillgo@gmail.com>
2019-02-18 04:25:32 -0500

    Revert "reduce time for no tag"
    This reverts commit 72ef7e3e542f0105c9a06c2b278216302ad5b9fc.

--- event/main_listener.php
+++ event/main_listener.php
@@ -183,8 +183,7 @@ class main_listener implements EventSubscriberInterface
 				$tags = $this->get_tags_from_post_request();
 				$all_tags = $this->tags_manager->split_valid_tags($tags);
 				$valid_tags = $all_tags['valid'];
-				if (count($valid_tags)>0)
-					$this->tags_manager->assign_tags_to_topic($data['topic_id'], $valid_tags);
+				$this->tags_manager->assign_tags_to_topic($data['topic_id'], $valid_tags);
 			}
 		}
 	}
